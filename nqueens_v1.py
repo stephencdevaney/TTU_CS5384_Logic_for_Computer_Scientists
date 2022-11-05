cdba018375eba6ac0a0b3b8409a3fc8623d35498
stephencdevaney <stephen.c.devaney@ttu.edu>
2022-11-05 15:56:37 -0500

    Created V2 working on running sat solver

--- nqueens_v1.py
+++ nqueens_v1.py
@@ -3,8 +3,29 @@
 # SPECIFICATION: Generate the DIMACS CNF for the n-queens problem based on user input
 # FOR: CS CS5384 Logic for Computer Scientists
 
+
+
 import os
-    
+
+
+
+##################### Functions
+def at_least_one(propositional_list):
+    return_clause = ""
+    for propositional_letter in propositional_list:
+        return_clause += str(propositional_letter) + " "
+    return return_clause + "0\n"
+
+def at_most_one(propositional_list):
+    return_clause = ""
+    return_count = 0
+    for i in range(0, len(propositional_list)):
+        for j in range(i+1, len(propositional_list)):
+            return_clause += "-" + str(propositional_list[i]) + " -" + str(propositional_list[j]) + " 0\n"
+            return_count += 1
+    return return_clause, return_count
+
+
 
 ##################### MAIN PROGRAM
 # Recieve User Input (thinking of using both from console and from command line)
@@ -15,77 +36,74 @@ total_spaces = n * n
 # Open clause output file
 with open("nqueens_clauses.txt", "w") as clause_temp_file:
 
-
     # GENERATE ROWS
     for i in range(1, total_spaces+1, n):
-        temp_clause = ""
+        p_list = []
         for j in range(i, n+i):
-            temp_clause += str(j) + " "
-        clause_temp_file.write(temp_clause + "0\n")
-        clause_count += 1
-        for j in range(i, n+i):
-            for k in range(j+1, n+i):
-                clause_temp_file.write("-" + str(j) + " -" + str(k) + " 0\n")
-                clause_count += 1
+            p_list.append(j)
+        clause_temp_file.write(at_least_one(p_list))
+        clause, temp_count = at_most_one(p_list)
+        clause_count += 1 + temp_count
+        clause_temp_file.write(clause)
 
     # GENERATE COLUMNS
     for i in range(1,  n+1):
+        p_list = []
         temp_clause = ""
         for j in range(i, total_spaces+i, n):
-            temp_clause += str(j) + " "
-        clause_temp_file.write(temp_clause + "0\n")
-        clause_count += 1
-        for j in range(i, total_spaces+i, n):
-            for k in range(j+n, total_spaces+i, n):
-                clause_temp_file.write("-" + str(j) + " -" + str(k) + " 0\n")
-                clause_count += 1
+            p_list.append(j)
+        clause_temp_file.write(at_least_one(p_list))
+        clause, temp_count = at_most_one(p_list)
+        clause_count += 1 + temp_count
+        clause_temp_file.write(clause)
 
     # GENERATE DIAGONALES
-    # idea 1
     for i in range(1, n+1):
+        p_list = []
+        clause = ""
         for j in range(i, total_spaces+1, n-1):
             if(j == i*n):
                 break
-            for k in range(j + n-1, total_spaces+1, n-1):
-                if(k == i*n):
-                    break
-                clause_temp_file.write("-" + str(j) + " -" + str(k) + " 0\n")
-                clause_count += 1
+            p_list.append(j)
+        temp_clause, temp_count = at_most_one(p_list)
+        clause += temp_clause
+        clause_count += temp_count
+        p_list = []
         for j in range(i, total_spaces+1, n+1):
             if(j == i +(n+1)*(n+1-i)):
                 break
-            for k in range(j + n+1, total_spaces+1, n+1):
-                if(k == i +(n+1)*(n+1-i)):
-                    break
-                clause_temp_file.write("-" + str(j) + " -" + str(k) + " 0\n")
-                clause_count += 1
+            p_list.append(j)
+        temp_clause, temp_count = at_most_one(p_list)
+        clause += temp_clause
+        clause_count += temp_count
+        clause_temp_file.write(clause)
 
-    # need to modify this section to do the rest of the grid
     for i in range(total_spaces, total_spaces-n, -1):
-        if i != total_spaces:
-            for j in range(i, 0, -n-1):
-                if(j == i - (n+1) * (i-(n*(n-1))) ):
-                    break
-                for k in range(j-n-1, 0, -n-1):
-                    if(k == i - (n+1) * (i-(n*(n-1)))):
-                        break
-                    clause_temp_file.write("-" + str(j) + " -" + str(k) + " 0\n")
-                    clause_count += 1
+        clause = ""
         if i != total_spaces - n + 1:
+            p_list = []
             for j in range(i, 0, -n+1):
                 if(j == (i-(n*(n-1))-1) * n + 1 ):
                     break
-                for k in range(j - n+1, 0, -n+1):
-                    if(k == (i-(n*(n-1))-1) * n + 1 ):
-                        break
-                    clause_temp_file.write("-" + str(j) + " -" + str(k) + " 0\n")
-                    clause_count += 1
+                p_list.append(j)
+            temp_clause, temp_count = at_most_one(p_list)
+            clause += temp_clause
+            clause_count += temp_count
+        if i != total_spaces:
+            p_list = []
+            for j in range(i, 0, -n-1):
+                if(j == i - (n+1) * (i-(n*(n-1))) ):
+                    break
+                p_list.append(j)
+            temp_clause, temp_count = at_most_one(p_list)
+            clause += temp_clause
+            clause_count += temp_count
+            clause_temp_file.write(clause)
     clause_temp_file.close()
 
-
-# GENERATE FINAL OUTPUT FILE
+# GENERATE CNF OUTPUT FILE
 # open output file and reopen clause file
-with open("nqueens.txt", "w") as outfile:
+with open("nqueens_v1.cnf", "w") as outfile:
     with open("nqueens_clauses.txt", "r") as clause_temp_file:
         # Generate Starting Information for SatSolver
         outfile.write("c AUTHORS: Generated by nqueens.py that was developed by Stephen Devaney, Amir Faiyaz, and Ucchwas Talukder\n")
@@ -105,3 +123,6 @@ with open("nqueens.txt", "w") as outfile:
         clause_temp_file.close()
 outfile.close()
 os.remove("nqueens_clauses.txt")
+
+# RUN MINISAT
+os.system("minisat nqueens_v1.cnf -no-luby -rinc=1.5 -phase-saving=0 -rnd-freq=0.02")
